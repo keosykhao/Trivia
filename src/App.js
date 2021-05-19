48bcfcc33e70f3e14446c12a1678d2c0a6840597
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-05-19 10:49:06 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+// import Music from './components/Music';
+import Selctions from './components/Selections'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Trivia</h1>
+      <Selctions />
+     {/* <Music /> */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Music.jsx
@@ -0,0 +1,6 @@
+// import React from 'react'
+// import Embed from 'react-music-embed'
+ 
+// export default () => (
+//   <Embed url="https://music.apple.com/us/album/anime-opening-vol-2/1554130712" />
+// )
