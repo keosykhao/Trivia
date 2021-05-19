ee70756291dae394c6ef11a63a4730278894cd6d
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-05-19 13:56:23 -0400

    added name comp

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-
+import Name from './components/Name'
 import './App.css';
 // import Music from './components/Music';
 import Selctions from './components/Selections'
@@ -7,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Trivia</h1>
+      <Name />
       <Selctions />
      {/* <Music /> */}
     </div>
