fbb236efc63e21d965dc983e5b0764a8a5f5b54d
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-06-25 08:57:29 -0400

    Update TriviaForm.jsx

---
+++ src/components/TriviaForm.jsx
@@ -0,0 +1,14 @@
+
+
+import React, { Component } from 'react'
+
+ class TriviaForm extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default TriviaForm;
