942b86e6ec6a3299b80d3f8688ce601a66564e9e
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-06-25 08:39:51 -0400

    Update TriviaComponent.jsx

--- src/components/TriviaComponent.jsx
+++ src/components/TriviaComponent.jsx
@@ -7,12 +7,53 @@ export default class TriviaComponent extends Component {
                 triviaItems: [];
             }
         }
-
-    render() {
-        return (
-            <div>
-                
-            </div>
-        )
+        // get data from the api
+    componentDidMount(){
+        TriviaService.getCartItems().then((response)=>{
+            this.setState({
+                triviaItems : response.data
+            })
+        })
     }
-}
+
+    deleteTriviaItem(id){
+        axios.delete(`http//localhost:8080/question/${id}`)
+    }
+    // create our table
+    render(){
+        return(
+            <div>
+            <h1 className = 'textcenter'>Trivia</h1>
+            <table className = 'table table-striped'>
+                <thead>
+                    <tr>
+                        <td>Question</td>
+                        <td>Answer</td>
+                        <td>Publish</td>
+                        
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        this.state.triviaItems.map(
+                            triviaItems => 
+                                <tr key = {triviaItems.id}>
+                                    <td>{triviaItems.qustion}</td>
+                                    <td>{triviaItem.answer}</td>
+                        
+                                    <td>
+                                        {/* This will be a button for delete */}
+                                        <button onClick={()=>{this.deleteTriviaItem(triviaItems.id)}}>Delete Me </button>
+                                    </td>
+                                </tr>
+                            
+                            )
+                    }
+                </tbody>
+            </table>
+            </div>
+)
+        }
+    
+export default TriviaComponent;
+
