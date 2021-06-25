76f6a83b281d2bf8f77ab33f2c97e8cf27b1122a
Ann Keosykhao <72222180+keosykhao@users.noreply.github.com>
2021-06-25 08:33:19 -0400

    Update TriviaComponent.jsx

--- src/components/TriviaComponent.jsx
+++ src/components/TriviaComponent.jsx
@@ -1,8 +1,13 @@
 import React, { Component } from 'react'
 
 export default class TriviaComponent extends Component {
+        constructor(){
+            super()
+            // this is the data from our db
+                triviaItems: [];
+            }
+        }
 
-    
     render() {
         return (
             <div>
